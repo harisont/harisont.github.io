58f4bf1e8faef80a54ac606a2a7aec310c331a38
Arianna Masciolini <uzkamascio@gmail.com>
2023-12-27 00:23:05 +0100

    edit graph and add to post

--- assets/img/laconici/langraph.py
+++ assets/img/laconici/langraph.py
@@ -3,5 +3,6 @@ import matplotlib.pyplot as plt
 langs = ["en", "es", "it", "sv", "da"]
 counts = [11, 2, 17, 15, 1]
 
-plt.pie(counts, labels = langs, startangle = 90)
-plt.show()
+fig = plt.figure()
+plt.pie(counts, labels = langs, startangle = 90, labeldistance=0.82)
+fig.savefig('2023.png', transparent=True)
