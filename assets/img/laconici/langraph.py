26e3ab61305111222cee5b7cb0680f80c2542fc5
Arianna Masciolini <uzkamascio@gmail.com>
2023-12-29 13:33:55 +0100

    update laconica

--- assets/img/laconici/langraph.py
+++ assets/img/laconici/langraph.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 
 langs = ["en", "es", "it", "sv", "da"]
-counts = [11, 2, 17, 15, 1]
+counts = [11, 2, 18, 15, 1]
 
 fig = plt.figure()
 plt.pie(counts, labels = langs, startangle = 90, labeldistance=0.82)
